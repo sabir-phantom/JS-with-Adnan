0c5467a8900b1a986851fd1c0f9c23dc8210a87d
sabir-phantom <sabir.sbair@gmail.com>
2023-04-14 02:06:39 +0600

    templates

---
+++ templates.js
@@ -0,0 +1,36 @@
+
+// let price = 40;
+
+// let text = "Hello World, " + price + " Greetings from Sabir";
+
+// console.log(text);
+
+let fname = "Mashkur Alam Sabir"
+let classIn = 10
+let age = 27
+
+// let text = "My name is " + fname  + " I read in class " + classIn + "My age is " + age + "."
+// console.log(text);
+
+let text = `My name is ${fname}. I read in class ${classIn}. My age is ${age}.`
+console.log(text);
+
+
+// total 7000tk
+// fish- 1200
+// beef- 4000
+// total cost???
+
+let total = 7000;
+let fish_price = 1200;
+let beef_price = 4000;
+
+// left money
+console.log(`how much money is left: ${total - (fish_price + beef_price)}`);
+
+
+// // toFixed()
+
+// let num = 5.56789
+// console.log(`Number: ${num.toFixed(2)}.
+// Variable Type: ${typeof num}`);
