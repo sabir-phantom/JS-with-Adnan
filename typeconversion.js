1329e7528042e09a98a9e7470e44f196bb54e2a9
sabir-phantom <sabir.sbair@gmail.com>
2023-04-14 02:06:50 +0600

    type converstion

---
+++ typeconversion.js
@@ -0,0 +1,24 @@
+// String to Number
+// 1. parseInt()
+// 2. parseFloat()
+// 3. Number()
+
+// let roll = "575"
+// console.log(typeof(roll));
+
+// roll = Number("sa");
+// console.log(roll);
+// console.log(typeof roll);
+
+
+// //  Number to String 
+// // 1. String()
+// // 2. toString()
+
+// let a = 100
+// console.log("before converting: " + typeof a);
+
+// a = String(a)
+// // a = a.toString()
+
+// console.log("after converting: " + typeof a);
