7d6b0769acf4fc0cf3b7658aeef23983ba593c22
sabir-phantom <sabir.sbair@gmail.com>
2023-04-14 02:05:43 +0600

    array

---
+++ array.js
@@ -0,0 +1,44 @@
+// ~~~Array Methods ~~~ //
+// 1. array length
+// 2. push() / unshift()
+// 3. pop() / shift()
+// 4. Array.isArray()
+// 5. splice() / slice()
+// 6. revers()
+// 7. sort()
+
+let cars = ["volvo", "toyota", 'honda'] //array
+// console.log(typeof(cars)); //object
+// console.log(cars.length);  //length
+
+// let cars_length = cars.length;
+// let last_element = cars[cars_length-1]; //last element
+
+// console.log();
+
+// cars.push("bmw");
+// console.log(cars);
+
+// console.log(Array.isArray(cars)); // Array checking
+
+// console.log(cars.pop()); //pop() remove last element
+// console.log(cars);
+
+// cars.shift(); // remove 1st element
+// console.log(cars);
+
+// cars.unshift("Ferrari");
+// console.log(cars);
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2, 0, "Lemon", "Kiwi"); //add elements
+
+// console.log(fruits);
+
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 3);
+// console.log(citrus);
+// console.log(fruits);
+
