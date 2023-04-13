d83890fc6dbd2725500e4f9100024fea5d5fbf6a
sabir-phantom <sabir.sbair@gmail.com>
2023-04-14 02:06:16 +0600

    strings + string search

---
+++ string_search.js
@@ -0,0 +1,17 @@
+// 1. indexOf()
+// 2. lastIndexOf()
+// 3. includes()
+// 4. startswith()
+// 5. endsWith()
+
+let myText = "Please locate where locate occurs"
+
+console.log(myText.indexOf("locate"));
+
+console.log(myText.lastIndexOf("locate"));
+
+console.log(myText.includes("locate"));
+
+console.log(myText.startsWith("Please"));
+
+console.log(myText.endsWith("occurs"));
---
+++ strings.js
@@ -0,0 +1,89 @@
+// ~~~String~~~ 
+
+// length
+// let t = "this is a dummy string"
+// let t_len = t.length
+// console.log(t_len);
+
+
+// String methods //
+// 1. slice(start, end)
+// 2. substring(start, end)
+// 3. substr(start, end)
+// 4. replace()
+// 5. replaceAll()
+// 6. toUpperCase()
+// 7. toLowerCase()
+// 8. concat()
+// 9. trim() / trimStart() / trimEnd()  //removes white space
+// 10. split()
+// 11. charAt()
+
+
+// let car = ["Toyota", "BMW", "Honda", "Nissan"];
+// // console.log(car[0]);
+// // console.log(car[1]);
+// // console.log(car[2]);
+// console.log("Before: " + car);
+
+// car[0] = "Tarzan"
+// console.log("after: " + car);
+
+
+// let t = "this is a dummy string"
+// let len = t.length
+
+// for(let i = 0; i < len; i++) {
+//     console.log(t[i]);
+// }
+
+
+// 1. slice(start, end)
+// let new_text = t.slice(0,13)
+// console.log(new_text);
+
+
+// 2. substring(start, end)
+// new_text = t.substring(0,13) // substr()
+// console.log(new_text);
+
+
+// 3. substr(start, end)
+// new_text = t.substr(0,13) 
+// console.log(new_text);
+
+
+// 4. replace() // // 5. replaceAll()
+// let text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+// new_text = text.replaceAll("Lorem","Adnan")
+// console.log(new_text);
+
+
+// 6. toUpperCase()
+// console.log(t.toUpperCase()); //
+// 7. toLowerCase()
+// console.log(t.toLowerCase()); //
+
+
+// 8. concat()
+// let msg = "Hello world"
+// let n = "Sabir"
+
+// // let text = msg.concat(" ", n, " and " + "AI")
+// let text = msg + " " + n + " " + "and" + " " + "AI"
+// console.log(text);
+
+
+
+
+// 10. split()
+// console.log(t);
+// console.log(typeof t);
+
+// let nt = t.split(" ")
+// console.log(nt);
+
+
+
+// 11. charAt()
+// console.log(t.charAt(0));
