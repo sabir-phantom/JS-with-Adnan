4e710dc4a5468900b2b6e2f60bc27b9eb00b8a6b
sabir-phantom <sabir.sbair@gmail.com>
2023-04-14 02:06:28 +0600

    function basic

---
+++ functions.js
@@ -0,0 +1,46 @@
+// ~~~ FUNCTIONS ~~~ //
+
+// function add(a, b) {
+//     //statement
+//     let sum = a + b;
+//     return sum; 
+// }
+
+//call
+// let result = add(7,5); //sum
+// console.log(result);
+
+
+// function printStudentDetails(name, id, age){
+//     console.log("Name: "+name);
+//     console.log("ID: "+id);
+//     console.log("Age: "+age);
+// }
+
+// printStudentDetails("Sabir", 1046, 28);
+
+
+// annonymous functions
+// let profile = function printStudentDetails(name, id, age){
+//     console.log("Name: "+name);
+//     console.log("ID: "+id);
+//     console.log("Age: "+age);
+// }
+
+// profile("Sabir", 1046, 28);
+// console.log(profile);
+
+
+
+// function add(m,n){
+//     return m+n;
+// }
+
+// console.log(add(5,6));
+
+// let result = add; // function reference
+
+
+
+
+// Callback function
