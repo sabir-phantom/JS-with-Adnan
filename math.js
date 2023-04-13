362a8a5cadca5a5e525bb0f877f701c2c434cc76
sabir-phantom <sabir.sbair@gmail.com>
2023-04-14 02:05:54 +0600

    math

---
+++ math.js
@@ -0,0 +1,47 @@
+// ~~~ MAth Objects ~~~ //
+
+//  4 methods
+// 1. Math.round(x)
+// 2. Math.ceil(x)
+// 3. Math.floor()
+// 4. Math.trunc()
+
+// console.log(Math.round(4.4)); // output-4
+// console.log(Math.round(4.5)); // output- 5
+
+// console.log(Math.ceil(4.1)); // output- 5
+// console.log(Math.floor(4.9)); // output- 4
+
+// console.log(Math.trunc(11.9)); // output- 11
+
+// // square
+// console.log("square root "+ Math.sqrt(25)); // output- 5
+
+// // absolute
+// console.log("absolute " + Math.abs(-5)); // output- 5
+
+
+
+
+
+// ~~~ Math.random ~~~ //
+// Math.random() always returns a number between 0 and 1;
+
+
+// 0 to 1 by default
+// let random_number = Math.random();
+// console.log(random_number);
+
+// // 0 to 9
+// let new_num = Math.trunc(random_number * 10);
+// console.log(Math.random() * 10);
+// console.log(new_num);
+
+// // 0 to 10
+// console.log();
+
+
+// toFixed()
+// let num = 5.56789
+// console.log(`Number: ${num.toFixed(2)}.
+// Variable Type: ${typeof num}`);
